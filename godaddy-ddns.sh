3c0b6c4f866683dd56c71c7f80c3bd3e32d30154
Jay Newstrom <jaynewstrom@gmail.com>
2019-11-29 09:34:07 -0600

    Make the check URL use HTTPS.

--- godaddy-ddns.sh
+++ godaddy-ddns.sh
@@ -15,10 +15,10 @@ rm -f "$SUCCESS_FILE"
 #
 # History:
 # v1.0 - 20160513 - 1st release.
-# 
+#
 # PS: Feel free to distribute but kindly retain the credits (-:
 ###########################################
- 
+
 # Begin settings
 # Get the Production API key/secret from https://developer.godaddy.com/keys/.
 # Ensure it's for "Production" as first time it's created for "Test".
@@ -27,7 +27,7 @@ Secret=$GODADDY_SECRET
 
 # Domain to update.
 Domain=$GODADDY_DOMAIN
- 
+
 # Advanced settings - change only if you know what you're doing :-)
 # Record type, as seen in the DNS setup page, default A.
 if [ -z "$GODADDY_TYPE" ];then
@@ -46,13 +46,13 @@ fi
 # Time To Live in seconds, minimum default 600 (10mins).
 # If your public IP seldom changes, set it to 3600 (1hr) or more for DNS servers cache performance.
 TTL=600
- 
+
 # Writable path to last known Public IP record cached. Best to place in tmpfs.
 CachedIP=/tmp/current_ip
- 
+
 # External URL to check for current Public IP, must contain only a single plain text IP.
-# Default http://api.ipify.org.
-CheckURL=http://api.ipify.org
+# Default https://api.ipify.org.
+CheckURL=https://api.ipify.org
 # End settings
 
 function onfail {
@@ -63,7 +63,7 @@ function onsuccess {
    touch "$SUCCESS_FILE"
    exit 0
 }
- 
+
 Curl=$(/usr/bin/which curl 2>/dev/null)
 [ "${Curl}" = "" ] &&
 echo "Error: Unable to find 'curl CLI'." && onfail
@@ -77,7 +77,7 @@ echo "Error: Requires 'Domain' value." && onfail
 [ "${TTL}" -lt 600 ] && TTL=600
 #/usr/bin/touch ${CachedIP} 2>/dev/null
 #[ $? -ne 0 ] && echo "Error: Can't write to ${CachedIP}." && exit 1
-[ -z "${CheckURL}" ] && CheckURL=http://api.ipify.org
+[ -z "${CheckURL}" ] && CheckURL=https://api.ipify.org
 echo -n "Checking current 'Public IP' from '${CheckURL}'..."
 PublicIP=$(${Curl} -kLs ${CheckURL})
 if [ $? -eq 0 ] && [[ "${PublicIP}" =~ [0-9]{1,3}\.[0-9]{1,3} ]];then
